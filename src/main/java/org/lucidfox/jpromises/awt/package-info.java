01be03bbee7b9d58b636cd78c409fb9a289eba77
Maia Everett <maia@everett.one>
2021-08-08 15:22:45 +0700

    Update developer email

--- src/main/java/org/lucidfox/jpromises/awt/package-info.java
+++ src/main/java/org/lucidfox/jpromises/awt/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Maia Everett <maia@lucidfox.org>
+ * Copyright 2014 Maia Everett <maia@everett.one>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
